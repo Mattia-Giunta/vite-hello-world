4ea8e827dfbf6374beabc0cc11a2a9740f21dd01
Mattia-Giunta <mattia.giunta.90@hotmail.com>
2024-02-06 18:46:35 +0100

    Creato repo e svolto esercizio

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+    // import
+
+    export default{
+        name: "AppTitle"
+    }
+
+</script>
+
+<template>
+
+    <h1 class="text-center">La mia prima app con Vite</h1>
+
+</template>
+
+<style scoped>
+
+    
+    
+</style>
